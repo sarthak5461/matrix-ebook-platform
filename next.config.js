ca3301428bed779009c4283b936540db09c9614e
sarthak sahu <sarthaksahu30@gmail.comm>
2026-07-31 18:18:00 +0530

    change next.config.json

--- next.config.js
+++ next.config.js
@@ -51,10 +51,10 @@ const nextConfig = {
       {
         source: "/(.*)",
         headers: [
-          {
-            key: "Content-Security-Policy",
-            value: csp,
-          },
+          // {
+          //   key: "Content-Security-Policy",
+          //   value: csp,
+          // },
           {
             key: "X-Frame-Options",
             value: "DENY",
